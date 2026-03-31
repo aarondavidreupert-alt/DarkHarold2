063a3fbbf2be4daac7c785f621848464493c6503
aarondavidreupert-alt <aaron.david.reupert@gmail.com>
2026-03-31 23:22:16 +0200

    soundon

--- convertAudio.py
+++ convertAudio.py
@@ -1,29 +1,26 @@
 import glob, os, subprocess
 
 def convertDir(inDir, outDir):
-	for path in glob.glob(os.path.join(inDir, "*.ACM")):
-		basename = os.path.splitext(os.path.basename(path))[0]
-		result = basename.lower() + ".wav" # output path of acm2wav
-		outpath = os.path.join(outDir, result)
+    os.makedirs(outDir, exist_ok=True)
+    for path in glob.glob(os.path.join(inDir, "*.[Aa][Cc][Mm]")):
+        basename = os.path.splitext(os.path.basename(path))[0]
+        result = basename.lower() + ".wav"
+        outpath = os.path.join(outDir, result)
 
-		print(path)
-		#print(basename)
+        print(path)
 
-		# convert to wav
-		#os.system("acm2wav %s" % path)
-		subprocess.call(["acm2wav", path], stdout=subprocess.PIPE)
+        subprocess.call(["acm2wav", path], stdout=subprocess.PIPE)
 
-		if not os.path.exists(result):
-			print("result file (%s) not found!" % result)
-			#break
-		elif not os.path.exists(outpath):
-			os.rename(result, outpath)
+        if not os.path.exists(result):
+            print("result file (%s) not found!" % result)
+        elif not os.path.exists(outpath):
+            os.rename(result, outpath)
 
 def main():
 	if not os.path.exists("acm2wav.exe"):
 		print("need acm2wav.exe")
 		return
-	if not os.path.exists("SFX"):
+	if not os.path.exists("data/sound/sfx"):
 		print("need SFX/")
 		return
 
@@ -36,8 +33,8 @@ def main():
 	if not os.path.exists("audio/music"):
 		os.mkdir("audio/music")
 
-	convertDir("SFX", "audio/sfx")
-	convertDir("sound/music", "audio/music")
+	convertDir("data/sound/sfx", "audio/sfx")
+	convertDir("data/sound/music", "audio/music")
 
 	print("done!")
 
